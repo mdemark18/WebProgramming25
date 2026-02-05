22866374d04a8f7395bcb99053be8f695e0d0ee2
Miller <147644013+mdemark18@users.noreply.github.com>
2026-02-05 09:52:40 -0700

    Update database connection details in db_connect.php
    Updated to work with InfinityFree

--- MillersGames/db_connect.php
+++ MillersGames/db_connect.php
@@ -1,7 +1,12 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "millersgames");
+$host = "sql206.infinityfree.com"; 
+$user = "if0_41067191";
+$pass = "prAFm3EWjahTor";
+$db   = "if0_41067191_millersgames";
+$conn = mysqli_connect("$host", "$user", "$pass", "$db");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+
